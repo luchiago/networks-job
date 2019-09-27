bd047b9778c89bfb9ec56f9a4c83ffe0c918a782
Isaias <isaigrec@gmail.com>
2019-09-27 08:15:46 -0300

    Adicionar toString para uPack

--- uPack.py
+++ uPack.py
@@ -8,3 +8,18 @@ class uPack:
         self.SYN = SYN
         self.FIN = FIN
         self.data = data
+    
+    # retorna o objeto em formato de string json
+    def toString(self):
+        msg = "{"
+        msg += "send_port: " + str(self.send_port) + ","
+        msg += "dest_port: " + str(self.dest_port) + ","
+        msg += "id_seq: " + str(self.id_seq) + ","
+        msg += "isAck: " + str(self.isAck) + ","
+        msg += "RST: " + str(self.RST) + ","
+        msg += "SYN: " + str(self.SYN) + ","
+        msg += "FIN: " + str(self.FIN) + ","
+        msg += "data: " + str(self.data) + ""
+        msg += "}"
+        
+        return msg
