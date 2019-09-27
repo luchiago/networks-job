ad7449cb4dcfc8aa961b8fa333768e73df2b0149
Isaias <isaigrec@gmail.com>
2019-09-27 08:30:18 -0300

    remover atributos de uPack, add sender

--- uPack.py
+++ uPack.py
@@ -1,12 +1,9 @@
 class uPack:
-    def __init__(self, send_port, dest_port, id_seq, isAck, RST, SYN, FIN, data):
+    def __init__(self, send_port, dest_port, id_seq, isAck, data):
         self.send_port = send_port
         self.dest_port = dest_port
         self.id_seq = id_seq
         self.isAck = isAck
-        self.RST = RST
-        self.SYN = SYN
-        self.FIN = FIN
         self.data = data
     
     # retorna o objeto em formato de string json
@@ -16,9 +13,6 @@ class uPack:
         msg += "dest_port: " + str(self.dest_port) + ","
         msg += "id_seq: " + str(self.id_seq) + ","
         msg += "isAck: " + str(self.isAck) + ","
-        msg += "RST: " + str(self.RST) + ","
-        msg += "SYN: " + str(self.SYN) + ","
-        msg += "FIN: " + str(self.FIN) + ","
         msg += "data: " + str(self.data) + ""
         msg += "}"
         
