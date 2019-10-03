08507ab303d7d93bc11da28a7f8a072ec95171b6
Isaias <isaigrec@gmail.com>
2019-10-03 19:31:15 -0300

    Adicionar timeout de pacotes

--- uSender.py
+++ uSender.py
@@ -5,6 +5,17 @@ import uPack
 SEG_SIZE = 100
 send_port = 4000
 dest_port = 5000
+send_ip = "localhost"
+
+send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def send_pack(uPack):
+    msg = uPack.toString()
+    msg_bytes = str.encode(msg)
+    send_sock.sendto(msg_bytes, (send_ip, send_port))
+    
 
 def sendAck(id_seq):
     uPack(send_port, dest_port, id_seq, True, None, None)
--- udp_client.py
+++ udp_client.py
@@ -1,4 +1,26 @@
 import socket
+from multiprocessing import TimeoutError
+from multiprocessing.pool import ThreadPool
+
+def timeout(seconds):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            pool = ThreadPool(processes=1)
+            result = pool.apply_async(function, args=args, kwds=kwargs)
+            try:
+                return result.get(timeout=seconds)
+            except TimeoutError as e:
+                return e
+        return wrapper
+    return decorator
+
+@timeout(5)
+def send_msg(msg_bytes, dest):
+    client_socket.sendto(msg_bytes, dest)
+    msg_bytes, client = client_socket.recvfrom(2048)
+    if msg_bytes.decode() == "ACK":
+        return msg_bytes.decode()
+    
 
 HOST = "localhost"
 PORT = 4000
@@ -11,11 +33,19 @@ servidor = (HOST, PORT)
 print("Cliente iniciado")
 
 while True:
+    ack = ""
     # recebendo a mensagem a ser enviada
     msg = input()
     # convertendo para bytes
     msg_bytes = str.encode(msg)
     # realizando o envio
-    client_socket.sendto(msg_bytes, servidor)
+    while ack != "ACK":
+        ack = send_msg(msg_bytes, servidor)
+        if isinstance(ack, TimeoutError):
+            print('Pacote perdido! Reenviando!')
+            ack = send_msg(msg_bytes, servidor)
+            continue
+        else:
+            print('Recebido ', ack)
 
 client_socket.close()
