418f72aaa3345c4c61cbbf67c1587e2d0e534254
Isaias <isaigrec@gmail.com>
2019-10-08 20:25:38 -0300

    att

--- uServer.py
+++ uServer.py
@@ -64,25 +64,7 @@ def send_msg(msg):
                 ack = True
        
     return True
- 
-### MAIN HERE ###
- 
-SEG_SIZE = 100
-prox_id = 0
-sender_port = 4000
-dest_port = 5000
-
-send_ip = input("IP destino: ")
-my_ip = input("meu ip: ")
- 
-send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# ativando o listen do servdor
-recv_sock.bind((my_ip, sender_port))
-recv_sock.settimeout(2)
- 
-last_pkt_id = 0
-
+    
 def send(msg):
     while True:
         if send_msg(msg):
@@ -112,6 +94,25 @@ def set_ip_dest(ip):
 def set_my_ip(ip):
     global my_ip
     my_ip = ip  
+
+### MAIN HERE ###
+ 
+SEG_SIZE = 100
+prox_id = 0
+sender_port = 4000
+dest_port = 5000
+
+send_ip = input("IP destino: ")
+my_ip = input("meu ip: ")
+ 
+send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# ativando o listen do servdor
+recv_sock.bind((my_ip, sender_port))
+recv_sock.settimeout(2)
+ 
+last_pkt_id = 0
+
 
 
 while True:
